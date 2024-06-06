5ff2d6b2b42d5705f1624c0f067a36486c22c126
Sergio rodriguez <rodriguezruizsergio@gmail.com>
2024-06-06 16:24:09 -0400

    can now add categories

--- app/controllers/concerns/http_authenticatable.rb
+++ app/controllers/concerns/http_authenticatable.rb
@@ -1,12 +1,10 @@
 module HttpAuthenticatable
   extend ActiveSupport::Concern
 
-  included do
-    before_action :authenticate
-  end
+
   def authenticate
     authenticate_or_request_with_http_basic('Administration') do |username, password|
-      username == .ENV['admin_username'] && password == ENV['admin_password']
+      username == ENV['admin_username'] && password == ENV['admin_password']
     end
   end
 end
